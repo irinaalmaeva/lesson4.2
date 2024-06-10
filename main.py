78a5b870366cd62c6de8a63b1021a74dda138f28
irinaalmaeva <almaeva.irina@gmail.com>
2024-06-10 23:51:21 +0300

    change class user

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ class User():
         self.access_level = access_level
         self.users = {}
 
-    def add_user(self, user_name, id, access_level):
-        self.users = {user_name: [id, access_level]}
-        print(f"Добавлен новый пользователь:{user_name}, {id}, {access_level}.")
+    def add_user(self, username, id, access_level):
+        self.users = {username: [id, access_level]}
+        print(f"Добавлен новый пользователь:{username}, {id}, {access_level}.")
 
-    def show_users(self):
-        print(self.users)
+    def show_users(self , self_users = None):
+        print(f"Добавлен пользователь: {self.users}")
 
     def remove_user(self, username):
         if username in self.users:
@@ -20,17 +20,26 @@ class User():
         print(f"Удален пользователь: {username}")
 
 
-class Admin(User):
-    def __init__(self, name, id, access_level, password):
-        super().__init__(name, id, access_level)
-        self.password = password
 
 
 
-user1 = User('Валерий', 22, 1)
-user2 = User('Мария', 33, 2)
-user3 = User('Ксения', 44, 1)
+user1 = User('Валерий', 22222, "финансист")
+user2 = User('Мария', 33333, "кадровик")
+user3 = User('Ксения', 44444, "расчетчик")
 
-user1.add_user('Анна', 55, 2)
-user2.add_user('Михаил',66,2)
-user3.remove_user('Ксения')
+print(f"Имя пользоавтеля: {user1.name}, имеет id: {user1.id} и уровень доступа: {user1.access_level}.")
+print(f"Имя пользоавтеля: {user2.name}, имеет id: {user2.id} и уровень доступа: {user2.access_level}.")
+print(f"Имя пользоавтеля: {user3.name}, имеет id: {user2.id} и уровень доступа: {user2.access_level}.")
+
+
+user1.add_user('Борис',66666,"финансист")
+user2.add_user('Анна', 55555, "кадровик")
+user3.add_user('Михаил',77777,"расчетчик")
+
+
+user1.show_users()
+user2.show_users()
+user3.show_users()
+
+user1.remove_user('Борис')
+
